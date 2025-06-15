cef3895f9933b4df919f2930d5acea02bfdfd7a1
sanabel <sanabelalrub@gmail.com>
2025-06-15 18:25:10 +0300

    final

--- DB/connection.js
+++ DB/connection.js
@@ -3,12 +3,13 @@ import mongoose from "mongoose";
 const connectDb = async () => {
     try {
         await mongoose.connect(process.env.DB, {
-           
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
         console.log("✅ Database connection established ✅");
     } catch (err) {
         console.error("❌ Failed to connect to MongoDB:", err);
-        process.exit(1);  
+        process.exit(1);
     }
 };
 
